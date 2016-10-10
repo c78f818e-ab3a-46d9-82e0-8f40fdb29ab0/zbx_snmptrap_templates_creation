3d1254c015eca94b8ea52d56daa6b4b323b1728c
Zubair AHMED <zubayr.a@gmail.com>
2016-10-10 16:05:43 +0530

    Renamed and changed steps points.

--- zabbix_installer_script/backup_scripts/history_table_clean_up_db_script.sh
+++ zabbix_installer_script/backup_scripts/history_table_clean_up_db_script.sh
@@ -25,7 +25,7 @@ echo "------------------------------------------"
 echo "##########################################"
 
 echo "------------------------------------------"
-echo "	Stopping Zabbix Server			"
+echo "	1. Stopping Zabbix Server			"
 echo "------------------------------------------"
 sudo service zabbix-server stop; 
 sleep 1
@@ -37,7 +37,7 @@ echo "show tables;" | mysql -u$ZABBIX_USER -p$ZABBIX_PASSWD $ZABBIX_DATABASE;
 sleep 1
 
 echo "------------------------------------------"
-echo "	Backing up ${ZABBIX_TABLE_NAME} Table.	"
+echo "	2. Backing up ${ZABBIX_TABLE_NAME} Table.	"
 echo "	Location : ${BACKUP_FILE_PATH}		"
 echo "------------------------------------------"
 mkdir -p ${BACKUP_DIR_PATH}
@@ -45,37 +45,37 @@ mysqldump -u$ZABBIX_USER -p$ZABBIX_PASSWD $ZABBIX_DATABASE ${ZABBIX_TABLE_NAME} 
 sleep 1
 
 echo "------------------------------------------------------------------"
-echo "1. Create Temp (${ZABBIX_TABLE_NAME}_${EPOCH_NOW}) Table"
+echo "  3. Create Temp (${ZABBIX_TABLE_NAME}_${EPOCH_NOW}) Table"
 echo "------------------------------------------------------------------"
 echo "CREATE TABLE ${ZABBIX_TABLE_NAME}_${EPOCH_NOW} LIKE ${ZABBIX_TABLE_NAME}; " | mysql -u$ZABBIX_USER -p$ZABBIX_PASSWD $ZABBIX_DATABASE;
 sleep 1
 
 echo "------------------------------------------------------------------"
-echo "2. Inserting from ${ZABBIX_TABLE_NAME} Table to Temp (${ZABBIX_TABLE_NAME}_${EPOCH_NOW}) Table"
+echo "  4. Inserting from ${ZABBIX_TABLE_NAME} Table to Temp (${ZABBIX_TABLE_NAME}_${EPOCH_NOW}) Table"
 echo "------------------------------------------------------------------"
 echo "INSERT INTO ${ZABBIX_TABLE_NAME}_${EPOCH_NOW} SELECT * FROM ${ZABBIX_TABLE_NAME} WHERE clock > '${EPOCH_3MONTHS_BACK}'; " | mysql -u$ZABBIX_USER -p$ZABBIX_PASSWD $ZABBIX_DATABASE;
 sleep 1
 
 echo "------------------------------------------------------------------"
-echo "3. Rename Table ${ZABBIX_TABLE_NAME} to ${ZABBIX_TABLE_NAME}_${EPOCH_NOW}_old"
+echo "  5. Rename Table ${ZABBIX_TABLE_NAME} to ${ZABBIX_TABLE_NAME}_${EPOCH_NOW}_old"
 echo "------------------------------------------------------------------"
 echo "ALTER TABLE ${ZABBIX_TABLE_NAME} RENAME ${ZABBIX_TABLE_NAME}_${EPOCH_NOW}_old;" | mysql -u$ZABBIX_USER -p$ZABBIX_PASSWD $ZABBIX_DATABASE;
 sleep 1
 
 echo "------------------------------------------"
-echo "4. Rename Temp Table (${ZABBIX_TABLE_NAME}_${EPOCH_NOW}) to Original Table (${ZABBIX_TABLE_NAME})"
+echo "  6. Rename Temp Table (${ZABBIX_TABLE_NAME}_${EPOCH_NOW}) to Original Table (${ZABBIX_TABLE_NAME})"
 echo "------------------------------------------"
 echo "ALTER TABLE ${ZABBIX_TABLE_NAME}_${EPOCH_NOW} RENAME ${ZABBIX_TABLE_NAME}; " | mysql -u$ZABBIX_USER -p$ZABBIX_PASSWD $ZABBIX_DATABASE;
 sleep 1
 
 echo "------------------------------------------"
-echo "5. Dropping Old Table (${ZABBIX_TABLE_NAME}_${EPOCH_NOW}_old), As we have already Backed it up. "
+echo "  7. Dropping Old Table (${ZABBIX_TABLE_NAME}_${EPOCH_NOW}_old), As we have already Backed it up. "
 echo "------------------------------------------"
 echo "DROP TABLE ${ZABBIX_TABLE_NAME}_${EPOCH_NOW}_old; " | mysql -u$ZABBIX_USER -p$ZABBIX_PASSWD $ZABBIX_DATABASE;
 sleep 1
 
 echo "------------------------------------------"
-echo "	Starting Zabbix Server		"
+echo "	8. Starting Zabbix Server		"
 echo "------------------------------------------"
 sudo service zabbix-server start;
 
